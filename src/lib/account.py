47eb14da6247a2c9671b0f35897865a6fb78d50f
rsz00 <andrv01icloud.com>
2025-02-19 19:09:25 +0100

    added arraygenerate

--- src/lib/account.py
+++ src/lib/account.py
@@ -2,4 +2,11 @@ class Account:
     def __init__(self,usrnm,psswd):
         self.username = usrnm
         self.password = psswd
-    
+
+#
+def generateAccountArray(names,psswords):
+    i = 0
+    ret = []
+    while i < len(names):
+        ret.append(Account(names[i],psswords[i]))
+    return ret
