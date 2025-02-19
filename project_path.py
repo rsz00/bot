35267b1a3e6247c32e2633998023f227973b8114
Kuehlregall <jan-lukas.dornieden@gmx.de>
2025-02-19 19:48:42 +0100

    test

---
+++ project_path.py
@@ -0,0 +1,2 @@
+import os
+project_path: str = os.path.abspath(os.path.dirname(__file__))
---
+++ src/lib/email.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+def emailName():
+    driver = webdriver.Firefox()
+    driver.get("https://www.google.com/")
+
+emailName()
+    
