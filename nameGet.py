471bdacdb204acce5f107295195ec23602c6fe63
Kuehlregall <jan-lukas.dornieden@gmx.de>
2025-02-19 18:54:13 +0100

    added getLast function

--- nameGet.py
+++ nameGet.py
@@ -16,8 +16,15 @@ def getFirst():
     with open(getPathFirst()) as file:
         first_names = file.readlines()
         first_names = [name.strip() for name in first_names]
-    randFirst = random.randint(1, 4944)
-    return first_names[randFirst]
+    randomNum = random.randint(1, 4944)
+    return first_names[randomNum]
+
+def getLast():
+    with open(getPathLast()) as file:
+        last_names = file.readlines()
+        last_names = [name.strip() for name in last_names]
+    randomNum = random.randint(1, 4944)
+    return last_names[randomNum]
 
 print(getFirst())
-    
+print(getLast())
