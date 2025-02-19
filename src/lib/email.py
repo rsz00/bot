57d1048c8f4493f3474c936158a0ed4f50c5b6a6
Kuehlregall <jan-lukas.dornieden@gmx.de>
2025-02-19 19:48:49 +0100

    test

--- src/lib/email.py
+++ src/lib/email.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 def emailName():
+    service = service()
     driver = webdriver.Firefox()
     driver.get("https://www.google.com/")
 
