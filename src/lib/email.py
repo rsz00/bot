a4b4616d6cd8d564c401431a2fd05d745555ecaa
Kuehlregall <jan-lukas.dornieden@gmx.de>
2025-02-19 20:17:17 +0100

    1

--- src/lib/email.py
+++ src/lib/email.py
@@ -1,11 +1,18 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
+import os
 
 def emailName():
-    service = service()
-    driver = webdriver.Firefox()
+    # Absoluten Pfad zum aktuellen Verzeichnis ermitteln
+    geckopath = os.path.abspath(os.path.dirname(__file__))
+    # Pfad zum Geckodriver angeben
+    service = Service(f"{geckopath}\\geckodriver.exe")
+    # Firefox-Webdriver mit dem angegebenen Service starten
+    driver = webdriver.Firefox(service=service)
+    # Webseite öffnen
     driver.get("https://www.google.com/")
+    # Browser schließen
+    driver.quit()
 
 emailName()
-    
