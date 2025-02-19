9250cfa4894dfaacd4032b75a7422d3ee47dc0d1
Kuehlregall <jan-lukas.dornieden@gmx.de>
2025-02-19 18:50:12 +0100

    added getFirst function

--- nameGet.py
+++ nameGet.py
@@ -1,4 +1,5 @@
 import os
+import random
 def getPathFirst():
     path = os.path.dirname(__file__)
     pathname = ("\cont\\first-names.txt")
@@ -11,4 +12,12 @@ def getPathLast():
     contdir = path + pathname
     return contdir
 
+def getFirst():
+    with open(getPathFirst()) as file:
+        first_names = file.readlines()
+        first_names = [name.strip() for name in first_names]
+    randFirst = random.randint(1, 4944)
+    return first_names[randFirst]
 
+print(getFirst())
+    
