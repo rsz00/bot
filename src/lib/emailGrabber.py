cc5860d6ad7ebd4a5430bddf5888dfbb9e5fe85b
Kuehlregall <jan-lukas.dornieden@gmx.de>
2025-02-20 17:24:55 +0100

    added emailGrabber.py

---
+++ src/lib/emailGrabber.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import re
+
+#grabs email
+def emailGrabber(driver):
+    urlEmail = "https://anonymmail.net/"
+    driver.get(urlEmail)
+    driver.implicitly_wait(7)
+    email = driver.find_element(By.CLASS_NAME, "email")
+    return email.text
+
+#gets confirmation code from email
+def emailConfirm(driver):
+    urlEmail = "https://anonymmail.net/"
+    driver.get(urlEmail)
+    driver.implicitly_wait(7)
+    emailText = driver.find_elements(By.XPATH, "/html/body/div[3]/div/div/div/div[1]/div[2]/div/div/div/div[2]/div[3]/a[2]/div/div[4]/b").text
+    emailCode = re.findall(r"\d+", emailCode)
+    return emailCode   
