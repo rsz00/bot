cebc4bdf657ec4b6f474f0ecb054c62ccb60f611
rsz00 <andrv01icloud.com>
2025-02-21 15:41:52 +0100

    edited gui

--- final_gui.py
+++ final_gui.py
@@ -1,10 +1,9 @@
 import tkinter as tk
+#from datainput import create_bots
 
-
-
-def get_value():
-    e = en.get()
-    print(e)
+#this creates the bots when button is pressed
+def do_the_thing():
+    
 
 root = tk.Tk()
 
@@ -15,8 +14,8 @@ root.maxsize(800,800)
 
 #struzcturing label
 tk.Label(root,text="Creation Amount: ").grid(row=0,column=0)
-en = tk.Entry(root)
-en.grid(row=0,column=2)
+e_num = tk.Entry(root)
+e_num.grid(row=0,column=2)
 
 tk.Button(root,text="Create",command=get_value).grid(row=0,column=3)
 
