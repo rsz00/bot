9f09ac81c7302aa4b1d78007188e2cdc5eda7ca3
Kuehlregall <jan-lukas.dornieden@gmx.de>
2025-02-21 14:33:53 +0100

    bug fixes, created randomMonth.py

--- lib/dataInput.py
+++ lib/dataInput.py
@@ -5,7 +5,6 @@ from lib import emailGrabber
 from lib import passGen
 from lib import nameGet
 from lib import credential_writer
-from lib import randomMonth
 from selenium.common.exceptions import *
 import time
 import random
