bec124b05cbc7a0cf7a787615d033413eab2d517
Kuehlregall <jan-lukas.dornieden@gmx.de>
2025-02-21 14:33:53 +0100

    bug fixes, created randomMonth.py

--- lib/dataInput.py
+++ lib/dataInput.py
@@ -4,8 +4,10 @@ from selenium.webdriver.common.keys import Keys
 from lib import emailGrabber
 from lib import passGen
 from lib import nameGet
+from lib import credential_writer
 from selenium.common.exceptions import *
 import time
+import random
 
 
 def dataInput(driver):
@@ -32,19 +34,23 @@ def dataInput(driver):
     driver.find_element(By.NAME, "fullName").send_keys(name[0] + " " + name[1])
 
     #input für username
-    try:
-        driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[1]/div/section/main/div/div/div[1]/div[2]/div/form/div[7]/div/div/div/button/span").click()
-    except NoSuchElementException:
-        driver.find_element(By.NAME, "username").send_keys(nameGet.genUsername(name[0], name[1]))
+    userName = nameGet.genUsername(name[0], name[1])
+    driver.find_element(By.NAME, "username").send_keys(userName)
     
+    #saves bot login
+    credential_writer.write_credentials(userName, password)
+
     #weiter click
     driver.implicitly_wait(5)
-    #for _ in range(3):
     buttons = driver.find_elements(By.TAG_NAME, "button")
     time.sleep(1)
     for button in buttons:
-        print(button.text)
         if button.text == "Weiter":
             button.click()
-            
+
+    #enter Birthdate
+    month = driver.find_elements(By.XPATH, "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div/section/main/div/div/div[1]/div[1]/div/div[4]/div/div/span/span[1]/select")
+    month.click()
+    month.send_keys(randomMonth[random.randint(1,12)])
+
 
